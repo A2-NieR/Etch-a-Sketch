e194ca1ecf4c099d04e8ade6bb62a8534c0a988b
Bunny <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-02-14 17:11:51 +0100

    Working grid with color changes

--- scripts.js
+++ scripts.js
@@ -1,24 +1,55 @@
-const btn = document.createElement('button');
-btn.innerHTML = 'Clear Grid';
-document.body.appendChild(btn).className = 'clear-grid';
+// Set up the fixed canvas
+const container = document.createElement('div');
+container.setAttribute('id', 'container');
+container.style.display = 'grid';
+container.style.margin = 'auto';
+container.style.height = '920px';
+container.style.width = '920px';
+document.body.appendChild(container);
 
-const container = document.getElementById("container");
+// Button setup & functionality
+const button = document.createElement('button');
+button.setAttribute('id', 'new-grid');
+button.innerHTML = 'Create new grid';
+button.style.margin = '25px auto';
+button.style.display = 'block';
+document.body.appendChild(button);
+document.body.insertBefore(button, container)
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    // cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
+button.addEventListener('click', () => {
+  const removeElements = (elms) => elms.forEach(el => el.remove());
+  removeElements( document.querySelectorAll(".cell") );
+  let colRow = window.prompt('Enter a number for the amount of columns/rows of the new grid: ', 16);
+  createGrid(colRow, colRow);
+});
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// The main function incl. color changer
+function createGrid(cols, rows) {
+  const dim = (cols * rows);
+  container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+
+  for (let i =0; i < dim; i++) {
+    const cell = document.createElement('div');
+    cell.setAttribute('id', (i+1));
+    cell.setAttribute('class', 'cell');
+    cell.style.height = '100%';
+    cell.style.width = '100%';
+    cell.style.padding = 'auto';
+    cell.style.border = '0.5px solid black';
+    container.appendChild(cell);
+
     cell.addEventListener('mouseover', function (e) {
-      e.target.style.background = 'black';
-    })
-  };
-};
+      let hue = getRandomIntInclusive(0, 360);
+      e.target.style.background = `hsl(${hue}, 100%, 50%)`;
+    });
+  }
+}
 
-makeRows(16, 16);
-
-document.body.insertBefore(btn, container);
-
-function clearGrid() {};
+createGrid(16, 16);
